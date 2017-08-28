5839de29698a4e84f5752e4c5b2d0f053b5477b4
Nim G <theredspoon@users.noreply.github.com>
2017-08-28 11:23:44 -0700

    ES6'd

--- makeCalculation.js
+++ makeCalculation.js
@@ -2,36 +2,37 @@ jQuery.noConflict();
 (function($) {
     "use strict";
     // USER-DEFINED SETTINGS (to be added to plugin settings)...needs an API call to get form fields
-    var displayAppRelatedRecordsFieldCode = "first_place_wins_rr";
+    let displayAppRelatedRecordsFieldCode = "first_place_wins_rr";
 
     // Once displayAppRelatedRecordsFieldCode is gotten in the plugin settings,
     // present options to match targetFieldCodeInDatasourceApp
     // The field code here to be type checked to a number
-    var targetFieldCodeInDatasourceApp = "num_points";
+    let targetFieldCodeInDatasourceApp = "num_points";
 
-    var lastCompleteSyncTimeStamp = null;
-    var outputFieldCodeInDisplayApp = "total_points";
-    var calculationFunction;
+    let lastCompleteSyncTimeStamp = null;
+    let outputFieldCodeInDisplayApp = "total_points";
+    let calculationFunction;
     // calculationFunction can be replaced with any function that
     // takes in many numbers and outputs one number
 
-    var sum = function(acc, curr) {
+    // possible calculation functions: sum, product, total count, count uniques, count nonempty values
+    const sum = function(acc, curr) {
         return Number(acc) + Number(curr);
     };
 
     calculationFunction = sum;
 
     // INTERNAL SETTINGS
-    var QUERYLIMIT = 500;
-    var appId = kintone.app.getId();
-    var datasourceAppId = "";
-    var makeCalculation = function(callback, array) {
+    const QUERYLIMIT = 500;
+    const appId = kintone.app.getId();
+    let datasourceAppId = "";
+    const makeCalculation = function(callback, array) {
         return array.reduce(function(acc, curr) {
             return callback(acc, curr);
         });
     };
 
-    var outputFieldValue = makeCalculation.bind(null, calculationFunction);
+    let outputFieldValue = makeCalculation.bind(null, calculationFunction);
 
     kintone.events.on([
         // "app.record.index.show", Not in API: 'app.record.index.show is not allowed to return "Thenable" object.'
@@ -41,43 +42,42 @@ jQuery.noConflict();
         "app.record.edit.submit",
         "mobile.app.record.edit.submit"
     ], function(event) {
-        var record = event.record;
+        const record = event.record;
 
         return kintone.api(kintone.api.url("/k/v1/app/form/fields", true), "GET", {"app": appId})
         .then(function(resp) {
 
             // organize the filter conditions
-            var properties = resp.properties;
-            var relatedRecordsField = properties[displayAppRelatedRecordsFieldCode];
-            var referenceTable = relatedRecordsField["referenceTable"];
+            const properties = resp.properties;
+            let relatedRecordsField = properties[displayAppRelatedRecordsFieldCode];
+            let referenceTable = relatedRecordsField["referenceTable"];
 
             datasourceAppId = referenceTable["relatedApp"]["app"];
 
-            var displayAppFetchConditionField = referenceTable["condition"]["field"];
-            var datasourceAppFetchConditionField = referenceTable["condition"]["relatedField"];
+            let displayAppFetchConditionField = referenceTable["condition"]["field"];
+            let datasourceAppFetchConditionField = referenceTable["condition"]["relatedField"];
 
-            var fetchCriteria = datasourceAppFetchConditionField + " = " +
-                record[displayAppFetchConditionField]["value"];
+            let fetchCriteria = `${datasourceAppFetchConditionField} = ${record[displayAppFetchConditionField].value}`;
 
-            var filterCond = referenceTable["filterCond"];
+            let filterCond = referenceTable["filterCond"];
 
-            var queryFilterSubstring = fetchCriteria;
+            let queryFilterSubstring = fetchCriteria;
             if (filterCond.length > 0) {
-                queryFilterSubstring = queryFilterSubstring + " and " + filterCond;
+                queryFilterSubstring = `${queryFilterSubstring} and ${filterCond}`;
             }
 
-            var requestParams = {
+            const requestParams = {
                 "fields": [targetFieldCodeInDatasourceApp],
                 "app": datasourceAppId,
-                // "query": see fetchAllRecords,
+                "query": queryFilterSubstring,
                 "totalCount": true
             };
 
-            var fetchAllRecords = function(params, filter, opt_offset, opt_records) {
-                var offset = opt_offset || 0;
-                var allRecords = opt_records || [];
+            const fetchAllRecords = function(params, filter, opt_offset, opt_records) {
+                let offset = opt_offset || 0;
+                let allRecords = opt_records || [];
 
-                params["query"] = filter + " limit " + QUERYLIMIT + " offset " + offset;
+                params["query"] = `${filter} limit ${QUERYLIMIT} offset ${offset}`;
 
                 return kintone.api(kintone.api.url("/k/v1/records", true), "GET", params).then(function(response) {
                     allRecords = allRecords.concat(response.records);
@@ -91,7 +91,7 @@ jQuery.noConflict();
             return fetchAllRecords(requestParams, queryFilterSubstring); // FIXME: JSWatchdog throwing an XSS warning here
         })
         .then(function(resp) {
-            var targetFieldValues = resp.map(function(rec) {
+            let targetFieldValues = resp.map(function(rec) {
                 return rec[targetFieldCodeInDatasourceApp]["value"];
             });
 
